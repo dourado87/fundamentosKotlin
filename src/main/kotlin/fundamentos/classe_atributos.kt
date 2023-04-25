33f30273fe01446a34ab85324137d0800eed1d76
Claudiomar Dourado <claudiodouradoti@gmail.com>
2023-04-24 21:13:44 -0300

    atributos e acessos

--- src/main/kotlin/fundamentos/classe_atributos.kt
+++ src/main/kotlin/fundamentos/classe_atributos.kt
@@ -1,9 +1,17 @@
 package fundamentos
 
-class Carro(var cor:String, var anoFabricacao:Int) {
+class Carro(var cor:String, var anoFabricacao:Int, var dono: Dono) {
+
+}
+data class Dono(var nome: String, var idade: Int){
 
 }
 
 fun main(){
-    var
+    var carro = Carro(cor = "Branco", anoFabricacao = 2021, Dono(nome= "Claudio", idade = 35))
+
+    println(carro.cor)
+    carro.cor = "Preto"
+    println(carro.cor)
+    println(carro.dono)
 }

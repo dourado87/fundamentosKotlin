58ae89e759f173ed2c9ef0b3c81d9ac4493a65ef
Claudiomar Dourado <claudiodouradoti@gmail.com>
2023-05-26 18:11:12 -0300

    aularepeticoes

--- src/main/kotlin/fundamentos/repeticoes.kt
+++ src/main/kotlin/fundamentos/repeticoes.kt
@@ -1,7 +1,11 @@
 package fundamentos
 
 fun main(){
-    printa1a10()
+//    printa1a10()
+//    printPar1a10()
+//    printRange(inicio = 10, fim = 20)
+//    whileMenorQue10()
+      doWhileMenorque10()
 }
 
 fun printa1a10(){
@@ -10,3 +14,38 @@ fun printa1a10(){
         println(numero)
     }
 }
+
+fun print10a1(){
+    for(numero in 10 downTo 1){
+        println(numero)
+    }
+}
+
+fun printPar1a10(){
+    for(numero in 2 .. 10 step 2){
+        println(numero)
+    }
+}
+
+fun printRange(inicio: Int, fim: Int){
+    for(numero in inicio .. fim){
+        println(numero)
+    }
+}
+
+fun whileMenorQue10(){
+    var x=0
+    while(x<10){
+        println(x)
+        x++
+    }
+}
+
+fun doWhileMenorque10(){
+    var x = 10
+    do{
+        println(x)
+        x++
+    }while(x<10)
+}
+
